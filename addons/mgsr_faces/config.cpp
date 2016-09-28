841c3aa5f1c85f92ecded41ae6f66c1515ac9cb4
Rebel <johnhartigan@ya.ru>
2016-09-29 00:48:33 +0300

    New naked and venom snake faces with custom heads.

--- addons/mgsr_faces/config.cpp
+++ addons/mgsr_faces/config.cpp
@@ -1,5 +1,21 @@
 #include "CfgPatches.hpp"
 
+class CfgHeads {
+	class NATOHead_A3;
+	
+	class mgsr_naked_head: NATOHead_A3 {
+		model = "\mgsr_faces\mgsr_naked_head.p3d";
+		selectionHeadWound = "injury_head";
+		selectionPersonality = "personality";
+	};
+	
+	class mgsr_venom_head: NATOHead_A3 {
+		model = "\mgsr_faces\mgsr_venom_head.p3d";
+		selectionHeadWound = "injury_head";
+		selectionPersonality = "personality";
+	};
+};
+
 class CfgFaces {
 	class Default
 	{
@@ -9,33 +25,36 @@ class CfgFaces {
 	class Man_A3: Default
 	{
 		class WhiteHead_01;
-		class WhiteHead_12;
 		
-		class mgsr_snakeface: WhiteHead_12
-		{
-			author = "Rebel / Shlomo";
-			displayname = "MGSR: Snakeface";
-			texture = "\mgsr_faces\data\mgsr_snakeface.paa";
-			material = "\mgsr_faces\data\mgsr_snake.rvmat";
-			identityTypes[] = {};
+		class mgsr_nakedsnake: WhiteHead_01 {
+			author="Rebel / Facel";
+			displayname = "MGSR: Naked Snake";
+			texture = "\mgsr_faces\data\mgsr_bb.paa";
+			materialWounded1 = "";
+			materialWounded2 = "";
+			head = "mgsr_naked_head";
+			material = "\mgsr_faces\data\mgsr_bb.rvmat";
+			identityTypes[] = {"Head_Naked_Snake"};
+		};
+	
+		class mgsr_venomsnake: WhiteHead_01 {
+			author="Rebel / Facel";
+			displayname = "MGSR: Venom Snake";
+			texture = "\mgsr_faces\data\mgsr_v.paa";
+			materialWounded1 = "";
+			materialWounded2 = "";
+			head = "mgsr_venom_head";
+			material = "\mgsr_faces\data\mgsr_v.rvmat";
+			identityTypes[] = {"Head_Venom_Snake"};
 		};
 		
 		class mgsr_skullface: WhiteHead_01
 		{
 			author = "Rebel / Shlomo";
-			displayname = "MGSR: Skullface";
+			displayname = "MGSR: Skull Face";
 			texture = "\mgsr_faces\data\mgsr_skullface.paa";
 			textureHL = "\mgsr_faces\data\mgsr_skullface_body.paa";
 			textureHL2 = "\mgsr_faces\data\mgsr_skullface_body.paa";
-			identityTypes[] = {};
-		};
-		
-		class mgsr_snakeface_camo: WhiteHead_12
-		{
-			author = "Rebel / Shlomo";
-			displayname = "MGSR: Snakeface Camo";
-			texture = "\mgsr_faces\data\mgsr_snakeface_camo.paa";
-			material = "\mgsr_faces\data\mgsr_snake_camo.rvmat";
 			identityTypes[] = {};
 		};
 	};
