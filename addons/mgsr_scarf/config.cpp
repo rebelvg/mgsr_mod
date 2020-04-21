3cf68ad1a7a65d55fcd6c8f63b2560ded08cfc7e
Facel <Room42@users.noreply.github.com>
2020-04-21 19:43:38 +0300

    Scarves
    Added scarves with color variations:
    Red
    Green
    Khaki
    Black

---
+++ addons/mgsr_scarf/config.cpp
@@ -0,0 +1,60 @@
+class CfgPatches {
+    class mgsr_scarf {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1;
+        #include "a3_addons.hpp"
+    };
+};
+
+class CfgGlasses {
+    class None;
+
+    class mgsr_scarf: None {
+        author = "Facel";
+        scope = 2;
+        displayName = "MGSR: Scarf (Green)";
+        model = "\mgsr_scarf\mgsr_scarf.p3d";
+        picture = "\mgsr_scarf\UI\gear_mgsr_scarf_01_x_ca.paa";
+	mass = 4;
+		identityTypes[]={};
+
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"\mgsr_scarf\data\mgsr_scarf_01_co.paa"};
+		hiddenSelectionsMaterials[] = {"\mgsr_scarf\data\mgsr_scarf.rvmat"};
+
+    };
+
+    class mgsr_scarf_khaki: mgsr_scarf {
+        author = "Facel";
+        scope = 2;
+        displayName = "MGSR: Scarf (Khaki)";
+        picture = "\mgsr_scarf\UI\gear_mgsr_scarf_02_x_ca.paa";
+		identityTypes[]={};
+
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"\mgsr_scarf\data\mgsr_scarf_02_co.paa"};
+    };
+
+    class mgsr_scarf_black: mgsr_scarf {
+        author = "Facel";
+        scope = 2;
+        displayName = "MGSR: Scarf (Black)";
+        picture = "\mgsr_scarf\UI\gear_mgsr_scarf_03_x_ca.paa";
+		identityTypes[]={};
+
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"\mgsr_scarf\data\mgsr_scarf_03_co.paa"};
+    };
+
+    class mgsr_scarf_red: mgsr_scarf {
+        author = "Facel";
+        scope = 2;
+        displayName = "MGSR: Scarf (Ocelot Red)";
+        picture = "\mgsr_scarf\UI\gear_mgsr_scarf_04_x_ca.paa";
+		identityTypes[]={};
+
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"\mgsr_scarf\data\mgsr_scarf_04_co.paa"};
+    };
+};
